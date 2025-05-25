ea57bd7bf958217550d0452caa715df2b9b5532b
Geekstrange <65814694+Geekstrange@users.noreply.github.com>
2025-05-25 15:47:55 +0800

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+const title = document.querySelector("#title");
+title.innerHTML = title.textContent
+	.replace(/\S/g, "<span>$&</span>")
+	.replace(/\s/g, "<span> </span>");
+let delay = 0;
+document.querySelectorAll("#title span").forEach((span, index) => {
+	delay += 0.1;
+
+	if (index === 6) delay += 0.3;
+
+	span.style.setProperty("--delay", `${delay}s`);
+});
+
+title.addEventListener("animationend", (e) => {
+	if (e.target === document.querySelector("#title span:last-child")) {
+		title.classList.add("ended");
+	}
+});
+//-------------------------------------Hiden Cards-------------------------------------------------------
+document.getElementById('switch').addEventListener('click', function() {
+	var blogSection = document.querySelector('section.blog');
+	if (blogSection.style.display === 'none' || blogSection.style.display === '') {
+		blogSection.style.display = 'block';
+	} else {
+		blogSection.style.display = 'none';
+	}
+});
+//-------------------------------------Copyright Year-----------------------------------------------------
+((ph = '%year%', cls = '.copy') =>
+	document.querySelector(cls).textContent =
+	document.querySelector(cls).textContent.replace(ph, new Date().getFullYear())
+)('%year%')
